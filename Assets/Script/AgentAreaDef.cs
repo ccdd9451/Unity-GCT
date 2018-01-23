6311bc147220c2323733c20b43e17929339e76a1
Hu Kaidong <hukaidonghkd@gmail.com>
2018-01-22 19:37:44 -0500

    initial commit

---
+++ Assets/Script/AgentAreaDef.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentAreaDef : MonoBehaviour {
+
+
+    public IList<float[]> agentArea = new List<float[]>();
+    public IList<int> weight = new List<int>();
+    public int agentAmount = 200;
+
+    IList<float> pArea;
+    enum AC {Xmin, Xmax, Zmin, Zmax}; //Area Coordination Index
+
+    GameObject sphere;
+
+
+    void Start() {
+        sphere = Resources.Load("Agent") as GameObject;
+        float s = 0;
+
+        pArea = weight.Select<int, float>(w => s += w).ToList();
+        pArea = pArea.Select(w => w / s).ToList();
+
+        for(int i = 0; i < agentAmount; i++)
+        {
+            Vector3 agentLoc = GenerateRandLoc();
+            Vector3 destLoc = GenerateRandLoc();
+
+            PlaceAgentOn(agentLoc, destLoc);
+        }
+    }
+
+    Vector3 GenerateRandLoc()
+    {
+        int trailCount = 0;
+        do
+        {
+            float rnd = UnityEngine.Random.value;
+            float xrnd = UnityEngine.Random.value;
+            float zrnd = UnityEngine.Random.value;
+
+            int indexedBlk = pArea
+                .Select(w => w > rnd)
+                .ToList().IndexOf(true);
+
+            float[] selArea = agentArea[indexedBlk];
+
+            float xpos = xrnd * (selArea[(int)AC.Xmax] - selArea[(int)AC.Xmin]) + selArea[(int)AC.Xmin];
+            float zpos = zrnd * (selArea[(int)AC.Zmax] - selArea[(int)AC.Zmin]) + selArea[(int)AC.Zmin];
+
+            if (!Physics.CheckSphere(new Vector3(xpos, 0.5f, zpos), 0.5f))
+            {
+                return new Vector3(xpos, 0f, zpos);
+            }
+
+            trailCount++;
+
+        } while (trailCount < 50);
+
+        throw new TimeoutException();
+    }
+
+
+    void PlaceAgentOn(Vector3 location, Vector3 destination)
+    {
+        GameObject agent = Instantiate(sphere) as GameObject;
+        agent.name = "Agent";
+        agent.transform.position = location;
+        agent.transform.parent = transform;
+
+        NavMeshAgent ag = agent.GetComponent<NavMeshAgent>();
+        ag.SetDestination(destination);
+    }
+}
---
+++ Assets/Script/XMLParser.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.IO;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEditor;
+
+public class XMLParser : MonoBehaviour
+{
+
+    [MenuItem("SteerSuite XML Parsing/Geometry Build")]
+    static void Build()
+    {
+
+        string XmlFilePath = "Assets/Resources/revit.xml";
+
+        string text = File.ReadAllText(XmlFilePath);
+        text = text.Replace("><", "> <");
+        File.WriteAllText(XmlFilePath, text);
+
+        GameObject cube = Resources.Load("Cube") as GameObject;
+        GameObject cylinder = Resources.Load("Cylinder") as GameObject;
+        GameObject plane = Resources.Load("Plane") as GameObject;
+        XmlReader reader = XmlReader.Create(XmlFilePath);
+
+        GameObject building = new GameObject("Building");
+        float groundXMin = 0.0f, groundXMax = 0.0f,
+              groundYMin = 0.0f, groundYMax = 0.0f,
+              groundZMin = 0.0f, groundZMax = 0.0f;
+
+        reader.ReadToFollowing("worldBounds");
+        while (true)
+        {
+            reader.Read();
+            if (reader.IsStartElement())
+            {
+                switch (reader.Name)
+                {
+                    case "xmin": groundXMin = reader.ReadElementContentAsFloat(); break;
+                    case "xmax": groundXMax = reader.ReadElementContentAsFloat(); break;
+                    case "ymin": groundYMin = reader.ReadElementContentAsFloat(); break;
+                    case "ymax": groundYMax = reader.ReadElementContentAsFloat(); break;
+                    case "zmin": groundZMin = reader.ReadElementContentAsFloat(); break;
+                    case "zmax": groundZMax = reader.ReadElementContentAsFloat(); break;
+                }
+            }
+            if (reader.NodeType == XmlNodeType.EndElement)
+            {
+                break;
+            }
+        }
+        GameObject ground = PrefabUtility.InstantiatePrefab(plane) as GameObject;
+        ground.name = "Ground";
+        ground.transform.position = new Vector3(groundXMax + groundXMin, groundYMax + groundYMin, groundZMax + groundZMin) / 2.0f;
+        ground.transform.localScale = new Vector3(groundXMax - groundXMin, groundYMax - groundYMin, groundZMax - groundZMin) / 20.0f;
+        ground.transform.parent = building.transform;
+        GameObjectUtility.SetStaticEditorFlags(ground, StaticEditorFlags.NavigationStatic);
+
+        while (reader.Read())
+        {
+            switch (reader.Name)
+            {
+                case "orientedBoxObstacle":
+                    {
+                        float thetaX = 0.0f, thetaY = 0.0f, thetaZ = 0.0f;
+                        float scaleX = 1.0f, scaleY = 1.0f, scaleZ = 1.0f;
+                        float posX_orientBox = 0.0f, posY_orientBox = 0.0f, posZ_orientBox = 0.0f;
+                        while (reader.Read() && reader.Name != "orientedBoxObstacle")
+                        {
+                            switch (reader.Name)
+                            {
+                                case "thetaX": thetaX = reader.ReadElementContentAsFloat(); break;
+                                case "thetaY": thetaY = reader.ReadElementContentAsFloat(); break;
+                                case "thetaZ": thetaZ = reader.ReadElementContentAsFloat(); break;
+                                case "size":
+                                    while (reader.Read() && reader.Name != "size")
+                                    {
+                                        switch (reader.Name)
+                                        {
+                                            case "x": scaleX = reader.ReadElementContentAsFloat(); break;
+                                            case "y": scaleY = reader.ReadElementContentAsFloat(); break;
+                                            case "z": scaleZ = reader.ReadElementContentAsFloat(); break;
+                                        }
+                                    }
+                                    break;
+                                case "position":
+                                    while (reader.Read() && reader.Name != "position")
+                                    {
+                                        switch (reader.Name)
+                                        {
+                                            case "x": posX_orientBox = reader.ReadElementContentAsFloat(); break;
+                                            case "y": posY_orientBox = reader.ReadElementContentAsFloat(); break;
+                                            case "z": posZ_orientBox = reader.ReadElementContentAsFloat(); break;
+                                        }
+                                    }
+                                    break;
+                            }
+                        }
+                        GameObject orientedBoxObstacle = PrefabUtility.InstantiatePrefab(cube) as GameObject;
+                        orientedBoxObstacle.name = "Oriented Box Obstacle";
+                        orientedBoxObstacle.transform.position = new Vector3(posX_orientBox, posY_orientBox + scaleY / 2.0f, posZ_orientBox);
+                        orientedBoxObstacle.transform.rotation = Quaternion.Euler(thetaX, thetaY, thetaZ);
+                        orientedBoxObstacle.transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
+                        orientedBoxObstacle.transform.parent = building.transform;
+                        GameObjectUtility.SetStaticEditorFlags(orientedBoxObstacle, StaticEditorFlags.NavigationStatic);
+                        break;
+                    }
+
+                case "obstacle":
+                    {
+                        float xmin = 0.0f, xmax = 0.0f, ymin = 0.0f, ymax = 0.0f, zmin = 0.0f, zmax = 0.0f;
+                        while (reader.Read() && reader.Name != "obstacle")
+                        {
+                            switch (reader.Name)
+                            {
+                                case "xmin": xmin = reader.ReadElementContentAsFloat(); break;
+                                case "xmax": xmax = reader.ReadElementContentAsFloat(); break;
+                                case "ymin": ymin = reader.ReadElementContentAsFloat(); break;
+                                case "ymax": ymax = reader.ReadElementContentAsFloat(); break;
+                                case "zmin": zmin = reader.ReadElementContentAsFloat(); break;
+                                case "zmax": zmax = reader.ReadElementContentAsFloat(); break;
+                            }
+                        }
+                        GameObject obstacle = PrefabUtility.InstantiatePrefab(cube) as GameObject;
+                        obstacle.name = "Obstacle";
+                        obstacle.transform.position = new Vector3(xmax + xmin, ymax + ymin, zmax + zmin) / 2.0f;
+                        obstacle.transform.localScale = new Vector3(xmax - xmin, ymax - ymin, zmax - zmin);
+                        obstacle.transform.parent = building.transform;
+                        GameObjectUtility.SetStaticEditorFlags(obstacle, StaticEditorFlags.NavigationStatic);
+                        break;
+                    }
+
+                case "circleObstacle":
+                    {
+                        float radius = 0.0f, height = 0.0f;
+                        float posX_circle = 0.0f, posY_circle = 0.0f, posZ_circle = 0.0f;
+                        while (reader.Read() && reader.Name != "circleObstacle")
+                        {
+                            switch (reader.Name)
+                            {
+                                case "radius": radius = reader.ReadElementContentAsFloat(); break;
+                                case "height": height = reader.ReadElementContentAsFloat(); break;
+                                case "position":
+                                    while (reader.Read() && reader.Name != "position")
+                                    {
+                                        switch (reader.Name)
+                                        {
+                                            case "x": posX_circle = reader.ReadElementContentAsFloat(); break;
+                                            case "y": posY_circle = reader.ReadElementContentAsFloat(); break;
+                                            case "z": posZ_circle = reader.ReadElementContentAsFloat(); break;
+                                        }
+                                    }
+                                    break;
+                            }
+                        }
+                        GameObject circleObstacle = PrefabUtility.InstantiatePrefab(cylinder) as GameObject;
+                        circleObstacle.name = "Circle Obstacle";
+                        circleObstacle.transform.position = new Vector3(posX_circle, posY_circle + height / 2.0f, posZ_circle);
+                        circleObstacle.transform.localScale = new Vector3(radius, height / 2.0f, radius);
+                        circleObstacle.transform.parent = building.transform;
+                        GameObjectUtility.SetStaticEditorFlags(circleObstacle, StaticEditorFlags.NavigationStatic);
+
+                        break;
+                    }
+
+                
+            }
+        }
+        //NavMeshBuilder.BuildNavMesh();
+    }
+    [MenuItem("SteerSuite XML Parsing/Config Agents")]
+    static void Parse()
+    {
+        string XmlFilePath = "Assets/Resources/revit.xml";
+
+        string text = File.ReadAllText(XmlFilePath);
+        text = text.Replace("><", "> <");
+        File.WriteAllText(XmlFilePath, text);
+        XmlReader reader = XmlReader.Create(XmlFilePath);
+
+        GameObject agentArea = new GameObject("agentArea");
+        AgentAreaDef agentAreaDef = agentArea.AddComponent<AgentAreaDef>();
+
+        reader.ReadToFollowing("worldBounds");
+        while (reader.Read())
+        {
+            switch (reader.Name)
+            {
+                case "agentRegion":
+                    {
+                        int numAgents = 1;
+                        float xmin = 0.0f, xmax = 0.0f, ymin = 0.0f, ymax = 0.0f, zmin = 0.0f, zmax = 0.0f;
+                        float x = 0.0f, y = 0.0f, z = 0.0f;
+                        while (reader.Read() && reader.Name != "agentRegion")
+                        {
+                            switch (reader.Name)
+                            {
+                                case "numAgents": numAgents = reader.ReadElementContentAsInt(); break;
+                                case "xmin": xmin = reader.ReadElementContentAsFloat(); break;
+                                case "xmax": xmax = reader.ReadElementContentAsFloat(); break;
+                                case "ymin": ymin = reader.ReadElementContentAsFloat(); break;
+                                case "ymax": ymax = reader.ReadElementContentAsFloat(); break;
+                                case "zmin": zmin = reader.ReadElementContentAsFloat(); break;
+                                case "zmax": zmax = reader.ReadElementContentAsFloat(); break;
+                                case "targetLocation":
+                                    while (reader.Read() && reader.Name != "targetLocation")
+                                    {
+                                        switch (reader.Name)
+                                        {
+                                            case "x": x = reader.ReadElementContentAsFloat(); break;
+                                            case "y": y = reader.ReadElementContentAsFloat(); break;
+                                            case "z": z = reader.ReadElementContentAsFloat(); break;
+                                        }
+                                    }
+                                    break;
+                            }
+
+                        }
+                        agentAreaDef.agentArea.Add(new float[] {xmin, xmax, zmin, zmax});
+                        agentAreaDef.weight.Add(numAgents);
+                        agentAreaDef.agentAmount = 100;
+                        break;
+                    }
+            }
+        }
+        Debug.Log(agentAreaDef.weight.Count.ToString());
+    }
+}
