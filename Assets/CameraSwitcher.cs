ccd8697e2f9cda3ba4e374810bdaa28d9a63ac1b
xz348 <xz348@scarletmail.rutgers.edu>
2018-01-31 22:41:58 -0500

    - New materials; - Camera switch control (by pressing 0, 1, 2, 3, and 4); - Lightmapping baking in progress;

---
+++ Assets/CameraSwitcher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour {
+
+    public Camera[] cameras;
+	
+	void Update () {
+        
+        for (int i = 0; i < cameras.Length; i++) {
+            if (Input.GetKeyDown(i.ToString())) {
+                for (int j = 0; j < cameras.Length; j++) {
+                    cameras[j].gameObject.SetActive(false);
+                }
+                cameras[i].gameObject.SetActive(true);
+                break;
+            }
+        }
+	}
+}
